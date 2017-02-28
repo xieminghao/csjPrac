6806d187befde317dd043b9b0de0e2d22fe1d296
liutongwang <liutongwang@xhqb.com>
2017-02-28 12:50:57 +0800

    change for dabao

---
+++ webpack/webpack-include-plugin.js
@@ -0,0 +1,97 @@
+var fs = require('fs');
+var path = require('path');
+var htmlmin = require('html-minifier');
+var _config = require('./config.js');
+
+//var _REGEX_STRING = /(<%%|<%=|<%-|<%_|<%#|<%|%>|-%>|_%>)/;
+var _REGEX_STRING = /(<%=|=%>)/;
+var _BOM = /^\uFEFF/;
+
+
+function webpackIncludePlugin(options) {
+    // Configure your plugin with options...
+}
+
+webpackIncludePlugin.prototype.apply = function(compiler) {
+    compiler.plugin('compilation', function(compilation) {
+        compilation.plugin('html-webpack-plugin-before-html-processing', function(htmlPluginData, callback) {
+            htmlPluginData.html = include(htmlPluginData.html);
+
+            //压缩HTML  API参考https://github.com/kangax/html-minifier
+            if(!_config.isDev){
+                htmlPluginData.html = htmlmin.minify(htmlPluginData.html, {
+                    collapseWhitespace: true
+                });
+            }
+            callback(null, htmlPluginData);
+        });
+    });
+};
+
+
+//从ejs提取出来的核心代码,分离include用
+function include(html){
+    var html, include, matches, htmlArr, ioHtml;
+    matches = parseTemplateText(html);
+    htmlArr = matches.concat();
+    matches.forEach(function (line, index) {
+        if ((include = line.match(/^\s*include\s+(\S+)/))) {
+            ioHtml = readFile(include[1]);
+            if(ioHtml){
+                htmlArr[index - 1] = '';
+                htmlArr[index] = ioHtml;
+
+                if(htmlArr.length > index + 1){
+                    htmlArr[index + 1] = '';
+                }
+            }
+        }
+    });
+    return htmlArr.join('');
+}
+
+//读取公共模版文件-缓存控制,优化性能
+function readFile(url){
+    var html;
+    try {
+        html = fs.readFileSync(path.join(__dirname, "../" + url)).toString().replace(_BOM, '');
+    } catch (e) {
+        //html = '';
+    }
+    console.log('url:', path.join(__dirname, "../" + url))
+    console.log('readFile html', html)
+    return html;
+}
+
+//对HTML使用正则表达式分割
+function parseTemplateText(html) {
+    var str = html
+        , result = _REGEX_STRING.exec(str)
+        , arr = []
+        , firstPos
+        , lastPos;
+
+    //Slurp spaces and tabs before <%_ and after _%>
+    str = str.replace(/[ \t]*<%_/gm, '<%_').replace(/_%>[ \t]*/gm, '_%>');
+    while (result) {
+        firstPos = result.index;
+        lastPos = _REGEX_STRING.lastIndex;
+
+        if (firstPos !== 0) {
+            arr.push(str.substring(0, firstPos));
+            str = str.slice(firstPos);
+        }
+
+        arr.push(result[0]);
+        str = str.slice(result[0].length);
+        result = _REGEX_STRING.exec(str);
+    }
+
+    if (str) {
+        arr.push(str);
+    }
+
+    return arr;
+}
+
+module.exports = webpackIncludePlugin;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,13 +4,14 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var webpackIncludePlugin = require('./webpack-include-plugin.js');
 var _config = require('./config.js');
 
 console.log('当前配置如下:', _config);
 
 module.exports = {
     entry: {
-        //'js/index' : [path.resolve(__dirname, '../src/index.js')]
+        // 'js/index' : [path.resolve(__dirname, '../src/index.js')]
     },
     output: {
         path: _config.output,
@@ -18,11 +19,24 @@ module.exports = {
         filename: '[name]-[hash:8].js',
         chunkFilename: 'js/[id]-[hash:8].chunk.js'
     },
+    module: {
+        // 加载器
+        loaders: [
+            { test: /\.vue$/, loader: 'vue' },
+            { test: /\.js$/, loader: 'babel', exclude: /node_modules/, query: {compact: false}},
+            { test: /\.css$/,loader: ExtractTextPlugin.extract('style-loader', 'css-loader')},
+            { test: /\.scss$/,loader: ExtractTextPlugin.extract('style-loader', "css-loader!autoprefixer-loader!sass-loader")},
+            { test: /\.(png|jpg|gif|bmp)$/, loader: 'url-loader?limit=8192&name=image/[name]-[hash:8].[ext]'},
+            { test: /\.(woff|woff2|eot|ttf|svg)(\??.*$|$)/, loader: 'url-loader?limit=100&name=fonts/[name]-[hash:8].[ext]'},
+            { test: /\.(html)$/, loader: 'html?-minimize' },
+        ],
+    },
     context: __dirname,
     node: {
         __dirname: true
     },
     plugins: [
+        new webpackIncludePlugin(),
         _config.isDev? _config.noop: new webpack.optimize.UglifyJsPlugin({   //压缩代码
             compress: {
                 warnings: false
@@ -35,9 +49,15 @@ module.exports = {
             }
         }),
         new CopyWebpackPlugin([{
-            from: (path.resolve(__dirname, '../src')),
-            to: (path.resolve(__dirname, '../output'))
+            from: (path.resolve(__dirname, '../src/script')),
+            to: (path.resolve(__dirname, '../output/script'))
         }]),
+        new HtmlWebpackPlugin({
+            filename: _config.htmlDir + 'view/index.html',    //生成的html存放路径
+            template: '../src/view/index.html',    //html模板路径
+            // chunks : ['js/index'],
+            inject: true,
+        })
     ],
     resolve: {
         modulesDirectories: ['node_modules'],
