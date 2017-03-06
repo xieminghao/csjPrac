f0b14d42783dd0bdcb7771e4a0c1e353e5945c06
xieminghao <527602660@qq.com>
2017-03-06 22:26:03 +0800

    2017/3/6 22:25

--- src/script/diyCtrl.js
+++ src/script/diyCtrl.js
@@ -1,10 +1,30 @@
-$.ajax({
-	url:'http://120.76.188.66:8080/pk10/',
-	data:{},
-	datatype:'html',
-	type:'get',
-	success:function(res){
-		$('#lot-content').html(res);
-		// drawTrend();
-	}
-});
+// 每个页面值调用一次的请求根据页面的location来判断请求的接口
+var host = 'http://120.76.188.66:8080'
+var pathname = location.pathname.replace('index.html','');
+console.log(pathname);
+
+if(pathname == '/pk10/'){
+	$.ajax({
+		url: host+pathname,
+		data:{},
+		datatype:'html',
+		type:'get',
+		success:function(res){
+			console.log(res);
+			$('#lot-content').html(res);
+			// drawTrend();
+		}
+	});
+} else {
+	$.ajax({
+		url: host+pathname,
+		data:{},
+		datatype:'html',
+		type:'get',
+		success:function(res){
+			console.log(res);
+			$('.lot-wrap').replaceWith(res);
+			console.log('replace completed');
+		}
+	});
+}
