e75e9175d7f28eb802f71acebf13eba41f934cc6
liutongwang <liutongwang@xhqb.com>
2017-03-14 22:45:37 +0800

    change for kaijiang

--- src/script/shishicai/index.js
+++ src/script/shishicai/index.js
@@ -4,12 +4,10 @@ var pathname = location.pathname.replace('index.html','');
 var lotMenu = 'cqssc_memu';
 var lotteryLuzhu = "";
 var successFn = function(res){
-    $('#lot-content').html(res);
-    //$('.feedbackDiv').remove();
+    $('.index-history').replaceWith(res);
+    $('.feedbackDiv').remove();
     //$('li[data-tag="zh"]').remove();
-    //$('li[data-tag="tbm"]').remove();
     //$('li[data-tag="sjyy"]').remove();
-    $("<scri"+"pt>"+"</scr"+"ipt>").attr({src:'../../script/navdrag.js',type:'text/javascript'}).appendTo($('body'));
 }
 
 $.ajax({
