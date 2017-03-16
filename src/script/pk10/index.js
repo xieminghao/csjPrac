04413a250f72808aa5d3e636f058858652ad944c
liutongwang <liutongwang@xhqb.com>
2017-03-16 18:46:28 +0800

    change for jianrong bug

--- src/script/pk10/index.js
+++ src/script/pk10/index.js
@@ -4,6 +4,7 @@ var pathname = location.pathname.replace('index.html','');
 console.log(host+pathname);
 
 var successFn = function(res){
+    //alert('req success!')
     $('#lot-content').html(res);
     // $('.feedbackDiv').remove();
     // $('li[data-tag="zh"]').remove();
@@ -11,7 +12,7 @@ var successFn = function(res){
     // $('li[data-tag="sjyy"]').remove();
     // $('li[data-tag="jq"]').remove();
 }
-
+//alert('req begin!')
 $.ajax({
     url: host+pathname,
     data:{},
@@ -19,3 +20,72 @@ $.ajax({
     type:'get',
     success: successFn
 });
+
+
+//$.ajax({
+//    url: host+pathname,
+//    method:'get',
+//    success: successFn
+//});
+
+function ajax (settings) {
+    var headers, name, transports, transport, i, length, xdrFlag = false;
+    headers = {
+        'X-Requested-With': 'XMLHttpRequest',
+        'Accept': 'text/javascript, text/html, application/xml, text/xml, */*',
+        "Content-Type": "application/json",
+        "data-type" : "json"
+    };
+
+    function createCORSRequest(){
+        var xhr;
+        try {
+            xhr = new XMLHttpRequest();
+        }catch(e){
+            xhr = new XDomainRequest();
+        }
+        if ("withCredentials" in xhr){
+
+        } else if (typeof XDomainRequest != "undefined"){
+            xhr = new XDomainRequest();
+            xdrFlag = true;
+        } else {
+            xhr = null;
+        }
+        return xhr;
+    }
+    var transport = createCORSRequest();
+    if(xdrFlag && typeof transport != 'undefined'){
+        // xdr,IE下
+        transport.open('get', settings.url, true);
+        transport.onload = function() {
+            alert(typeof(transport.responseText))
+            settings.success(transport.responseText)
+        };
+        transport.send();
+    }else if(typeof transport != 'undefined') {
+        transport.onreadystatechange = function () {
+            if (transport.readyState !== 4) {
+                return;
+            }
+            if (transport.readyState === 4 && transport.status >= 200 && transport.status < 400) {
+                settings.success(transport.responseText);
+            }else{
+                settings.fail(transport.responseText);
+            }
+        };
+        if(settings.timeout) {
+            transport.timeout = settings.timeout;
+        }
+        if(settings.method == 'get'){
+            transport.open('get', settings.url, true);
+            transport.send();
+        }else{
+            transport.open('post', settings.url, true);
+            for (name in headers) {
+                transport.setRequestHeader(name, headers[name]);
+            }
+            transport.send(JSON.stringify(settings.param));
+        }
+    }
+};
--- src/script/public/common.js
+++ src/script/public/common.js
@@ -38,3 +38,144 @@ jQuery.extend({
         });
     }
 });
+
+//function ajax (settings) {
+//    var headers, name, transports, transport, i, length, xdrFlag = false;
+//    headers = {
+//        'X-Requested-With': 'XMLHttpRequest',
+//        'Accept': 'text/javascript, text/html, application/xml, text/xml, */*',
+//        "Content-Type": "application/json",
+//        "data-type" : "json"
+//    };
+//
+//    function createCORSRequest(){
+//        var xhr;
+//        try {
+//            xhr = new XMLHttpRequest();
+//        }catch(e){
+//            xhr = new XDomainRequest();
+//        }
+//        if ("withCredentials" in xhr){
+//
+//        } else if (typeof XDomainRequest != "undefined"){
+//            xhr = new XDomainRequest();
+//            xdrFlag = true;
+//        } else {
+//            xhr = null;
+//        }
+//        return xhr;
+//    }
+//    var transport = createCORSRequest();
+//    if(xdrFlag && typeof transport != 'undefined'){
+//        // xdr,IE下
+//        transport.open('get', settings.url, true);
+//        transport.onload = function() {
+//            alert(typeof(transport.responseText))
+//            settings.success(transport.responseText)
+//        };
+//        transport.send();
+//    }else if(typeof transport != 'undefined') {
+//        transport.onreadystatechange = function () {
+//            if (transport.readyState !== 4) {
+//                return;
+//            }
+//            if (transport.readyState === 4 && transport.status >= 200 && transport.status < 400) {
+//                settings.success(transport.responseText);
+//            }else{
+//                settings.fail(transport.responseText);
+//            }
+//        };
+//        if(settings.timeout) {
+//            transport.timeout = settings.timeout;
+//        }
+//        if(settings.method == 'get'){
+//            transport.open('get', settings.url, true);
+//            transport.send();
+//        }else{
+//            transport.open('post', settings.url, true);
+//            for (name in headers) {
+//                transport.setRequestHeader(name, headers[name]);
+//            }
+//            transport.send(JSON.stringify(settings.param));
+//        }
+//    }
+//};
+//
+//
+//jQuery.extend({
+//    ajax: function(settings) {
+//        ajax(settings)
+//    },
+//    //HTTP get方法
+//    get: function (url, data, callback, type) {
+//        // shift arguments if data argument was ommited
+//        if (jQuery.isFunction(data)) {
+//            callback = data;
+//            data = null;
+//        }
+//        // debugger;
+//        return jQuery.ajax({
+//            type: "GET",
+//            url: HOST+url,
+//            data: $.extend({isa:true}, data),
+//            // headers: {
+//            //     // 'X-Requested-With': 'XMLHttpRequest',
+//            //     "Access-Control-Allow-Headers":"X-Requested-With"
+//            // },
+//            // beforeSend: function(xhr){
+//            //      xhr.setRequestHeader('X-Requested-With','XMLHttpRequest');
+//            // },
+//            success: callback,
+//            dataType: type
+//        });
+//    },
+//    post: function (url, data, callback, type) {
+//
+//        if (jQuery.isFunction(data)) {
+//            callback = data;
+//            data = {};
+//        }
+//        return jQuery.ajax({
+//            type: "POST",
+//            url: HOST+url,
+//            data: data,
+//            success: callback,
+//            dataType: type
+//        });
+//    }
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
