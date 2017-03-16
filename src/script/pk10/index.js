4737712e19d341cefdc04538e101c900940ff444
liutongwang <liutongwang@xhqb.com>
2017-03-16 18:54:37 +0800

    change for ajax

--- src/script/pk10/index.js
+++ src/script/pk10/index.js
@@ -1,7 +1,6 @@
 // 每个页面值调用一次的请求根据页面的location来判断请求的接口
 var host = 'http://120.76.188.66:8080'
 var pathname = location.pathname.replace('index.html','');
-console.log(host+pathname);
 
 var successFn = function(res){
     //alert('req success!')
