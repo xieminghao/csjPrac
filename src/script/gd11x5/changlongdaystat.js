2701169d8bccf5dd516d3a4f6cae282e2867c616
xieminghao <527602660@qq.com>
2017-03-18 16:49:53 +0800

    push some view bug fixed

--- src/script/gd11x5/changlongdaystat.js
+++ src/script/gd11x5/changlongdaystat.js
@@ -1,6 +1,6 @@
 // 每个页面值调用一次的请求根据页面的location来判断请求的接口
 var host = 'http://120.76.188.66:8080'
-var pathname = location.pathname.replace('index.html','');
+var pathname = '/gd11x5/changlongdaystat';
 var lotMenu = 'gd11x5_memu';
 var lotteryLuzhu = "";
 
--- src/script/gdkl10/changlongdaystat.js
+++ src/script/gdkl10/changlongdaystat.js
@@ -1,6 +1,6 @@
 // 每个页面值调用一次的请求根据页面的location来判断请求的接口
 var host = 'http://120.76.188.66:8080'
-var pathname = location.pathname.replace('index.html','');
+var pathname = '/gdkl10/changlongdaystat';
 var lotMenu = 'gdkl10_memu';
 
 function changeSelect() {
