9c9618813b4958108181d501b331c57214c680b5
liutongwang <liutongwang@xhqb.com>
2017-03-31 22:06:37 +0800

    change for bug

--- src/script/pk10/numberplan.js
+++ src/script/pk10/numberplan.js
@@ -1,6 +1,7 @@
 // 每个页面值调用一次的请求根据页面的location来判断请求的接口
-var pathname = location.pathname.replace('index.html','');
+// var pathname = location.pathname.replace('index.html','');
 //var lotteryLuzhu = "";
+var pathname = '/pk10/numberplan';
 
 $.ajax({
     url: host+pathname,
