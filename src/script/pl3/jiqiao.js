5c435a8deaff068d0a830fe190e70a8eae1157ce
xieminghao <527602660@qq.com>
2017-04-16 09:49:24 +0800

    0416 1st commit

---
+++ src/script/pl3/jiqiao.js
@@ -0,0 +1,18 @@
+// 每个页面值调用一次的请求根据页面的location来判断请求的接口
+var pathname = location.pathname.replace('index.html','');
+var lotteryLuzhu = "";
+
+$.ajax({
+    url: host+pathname,
+    data:{},
+    datatype:'html',
+    type:'get',
+    success: function(res){
+        $('.lot-wrap').replaceWith(res);
+
+    }
+});
+
+
+
+
