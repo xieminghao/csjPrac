c2f77fce2b116e9dc24b5a8036dedee8c162c073
liutongwang <liutongwang@xhqb.com>
2017-04-19 22:43:06 +0800

    change for shishicai

--- src/script/public/jiqiao_common.js
+++ src/script/public/jiqiao_common.js
@@ -1,8 +1,8 @@
-var pathname = location.pathname.replace('index.html','');
-pathname = pathname + 'article.html'
 var $method_list = $("ul.method>li>a");
 console.log('method_list:', $method_list);
 $method_list.click(function(e){
+    var pathname = location.pathname.replace('index.html','');
+    pathname = pathname + 'article.html'
     var target = e.target;
     var href = target.pathname;
     console.log('href:', href);
